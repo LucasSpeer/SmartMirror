47d6758b7b1d938b521d7ff396fd445a7552c76d
LucasSpeer <lucasspeer123@gmail.com>
2017-09-29 14:38:52 -0500

    removed weatherapp, changed index.html to basic weather widget

---
+++ main.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function() {
+  $.simpleWeather({
+    location: 'Austin, TX',
+    woeid: '',
+    unit: 'f',
+    success: function(weather) {
+      html = '<p>'+weather.temp+'&deg;'+weather.units.temp+'</p>';
+  
+      $("#weather").html(html);
+    },
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+  });
+});
