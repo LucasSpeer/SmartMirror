ec51a26e8c5589a2d1e827670185921e61968939
LucasSpeer <lucasspeer123@gmail.com>
2018-02-14 13:50:53 -0600

    added test files and updated main files

---
+++ www/test.js
@@ -0,0 +1,23 @@
+/*globals $:false */
+function loadWeather(location) {
+    'use strict';
+    $.simpleWeather({
+        location: location,
+        unit: 'f',
+        success: function (weather) {
+            var temp = weather.temp + '&deg;',
+				forecast = weather.forecast[3].text,
+                city = weather.city + ', ' + weather.region;
+            $(".location").text(city);			//assign .location class the text in city
+            $(".temperature").html(temp);
+			$(".forecast").text(forecast);
+        },
+        error: function (error) {
+            $(".location").text("Mankato, MN");
+            $(".temperature").html("69 f");
+        }
+    });
+}
+$( document ).ready(function () {
+	loadWeather('56001');
+});
