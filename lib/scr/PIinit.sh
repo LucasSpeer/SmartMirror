0b4aa78e3f5002890cd10a5a566f98d37116e0b3
LucasSpeer <lucasspeer123@gmail.com>
2018-02-15 01:23:36 +0000

    greeting, time, and weather work dynamically. Need to stylize. updated PIinit.sh to affect /var/www/html/

--- lib/scr/PIinit.sh
+++ lib/scr/PIinit.sh
@@ -3,6 +3,8 @@
 cd /home/pi/SmartMirror/lib/scr/
 sudo hciconfig hci0 up
 sudo hciconfig hci0 piscan &
+sudo cp rfcomm-server.py /var/www/html/
+sudo cp simple-agent /var/www/html/
+cd /var/www/html/
 sudo python rfcomm-server.py &
 sudo python simple-agent &
-chromium-browser localhost --kiosk &
