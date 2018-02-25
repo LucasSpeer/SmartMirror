f67a3382b9b8a82827ad0d0818714933e1a7ac69
root <lucasspeer123@gmail.com>
2018-02-25 20:54:22 +0000

    added autostart and updated firstBoot.sh to move it to its necessary spot

--- lib/scr/firstBoot.sh
+++ lib/scr/firstBoot.sh
@@ -10,6 +10,8 @@ echo "creating addr.save which contains the local BT mac address pulled from hci
 sudo python ~/SmartMirror/lib/rfcommset.py
 echo "creating /etc/bluetooth/rfcomm.conf"
 sudo cp rfcomm.conf /etc/bluetooth/
+sudo cp bluezutils.py /var/www/html/
+echo "moving rfcomm.conf and bluezutils into /var/www/html/"
 cd /home/pi/SmartMirror/lib/
 sudo cp main.conf /etc/bluetooth/
 echo "copying main.conf into /etc/bluetooth/"
@@ -19,3 +21,5 @@ sudo cp panel /home/pi/.config/lxpanel/LXDE-pi/panels/
 echo "copying the panel file into /home/pi/.config/lxpanel/LXDE-pi/panels/ to remove the confirm BT pair prompt"
 sudo cp dbus-org.bluez.service /etc/systemd/system/
 echo "copying dbus-org.bluez.service into /etc/systemd/system/ to run BT in comptability mode"
+sudo cp autostart /home/pi/.config/lxpanel/LXDE-pi/
+echo "copy autostart into /home/pi/.config/lxpanel/LXDE-pi/ to boot chrome on startup"
