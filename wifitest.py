2d69a7672b0090a54791e2b170c4694c157a0c97
root <lucasspeer123@gmail.com>
2018-03-28 09:44:13 -0500

    added a test python script to check for wifi and retrieve networks

---
+++ wifitest.py
@@ -0,0 +1,19 @@
+import urllib
+
+
+try:
+    url = "https://www.google.com"
+    urllib.urlopen(url)
+    status = "Connected"
+except:
+    status = "Not connected"
+if status == "Connected":
+	wifiFile = open('wifilist.save', 'r')
+	listStr = ""
+	for line in wifiFile:
+		tmp = line
+		sep = tmp.split("\"")
+		if "\\" not in sep[1]:
+			if (sep[1] + "\n") not in listStr:
+				listStr += sep[1] + "\n"
+	print(listStr)
