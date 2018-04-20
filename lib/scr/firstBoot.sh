b64840bb1b1259db49df2f07dcfebab08db68959
LucasSpeer <lucasspeer123@gmail.com>
2018-04-20 12:07:47 -0500

    debugged firstBoot.sh & added rfcomm-server.py into saveToServer.sh

--- lib/scr/firstBoot.sh
+++ lib/scr/firstBoot.sh
@@ -4,27 +4,28 @@
 # /etc/bluetooth/rfcomm.conf is set dynamically using th hciconfig line and the rfcommset.py sc
 # This file also creates a local directory and sets up the necesarry structures. Consider it a setup executable
 cd /home/pi
+echo "Creating local directory"
 sudo mkdir SmartMirror-1.0/
 sudo mkdir SmartMirror-1.0/SmartMirror/
-sudo cp SmartMirror/* SmartMirror-1.0/SmartMirror/ 
+sudo cp SmartMirror/* -r SmartMirror-1.0/SmartMirror/ 
 sudo chmod 777 /var/run/sdp
-echo "making /var/run/sdp executable"
+echo "Making /var/run/sdp executable"
 cd /home/pi/SmartMirror-1.0/SmartMirror/lib/scr/
 sudo hciconfig hci0 | grep Address | sudo nano addr
-echo "creating addr.save which contains the local BT mac address pulled from hciconfig, for use with rfcommset.py"
-sudo python ~/SmartMirror-1.0/SmartMirror/lib/rfcommset.py
-echo "creating /etc/bluetooth/rfcomm.conf"
+echo "Creating addr.save which contains the local BT mac address pulled from hciconfig, for use with rfcommset.py"
+sudo python ~/SmartMirror-1.0/SmartMirror/lib/scr/rfcommset.py
+echo "Creating /etc/bluetooth/rfcomm.conf"
 sudo cp rfcomm.conf /etc/bluetooth/
 sudo cp bluezutils.py /var/www/html/
-echo "moving rfcomm.conf and bluezutils into /var/www/html/"
+echo "Moving rfcomm.conf and bluezutils into /var/www/html/"
 cd /home/pi/SmartMirror-1.0/SmartMirror/lib/
 sudo cp main.conf /etc/bluetooth/
-echo "copying main.conf into /etc/bluetooth/"
+echo "Copying main.conf into /etc/bluetooth/"
 sudo cp rc.local machine-info /etc/
-echo "copying machine-info and the rc.local file that doesn't call this script into /etc/"
+echo "Copying machine-info and the rc.local file that doesn't call this script into /etc/"
 sudo cp panel /home/pi/.config/lxpanel/LXDE-pi/panels/
-echo "copying the panel file into /home/pi/.config/lxpanel/LXDE-pi/panels/ to remove the confirm BT pair prompt"
+echo "Copying the panel file into /home/pi/.config/lxpanel/LXDE-pi/panels/ to remove the confirm BT pair prompt"
 sudo cp dbus-org.bluez.service /etc/systemd/system/
-echo "copying dbus-org.bluez.service into /etc/systemd/system/ to run BT in comptability mode"
+echo "Copying dbus-org.bluez.service into /etc/systemd/system/ to run BT in comptability mode"
 sudo cp autostart /home/pi/.config/lxpanel/LXDE-pi/
-echo "copy autostart into /home/pi/.config/lxpanel/LXDE-pi/ to boot chrome on startup"
+echo "Copying autostart into /home/pi/.config/lxpanel/LXDE-pi/ to boot chrome on startup"
--- www/saveToServer.sh
+++ www/saveToServer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ~/SmartMirror/www
-sudo cp wifitest.py config.json index.html main.css time.js test.js /var/www/html/
+sudo cp rfcomm-server.py wifitest.py config.json index.html main.css time.js test.js /var/www/html/
 
